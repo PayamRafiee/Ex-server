752beab66d5af9018fd415b646294d483f7ab6b9
Payam Rafiee <payamrafiee@Payams-Mac-mini-2.local>
2018-06-10 15:16:36 +0300

    Setup for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -21,8 +22,8 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('started on port 3000');
+app.listen(port, () => {
+  console.log(`started on port ${port}`);
 });
 
 module.exports = {app};
