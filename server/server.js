ad1521cdbcb38c1520764b45895286e1f1805bb1
Payam Rafiee <payamrafiee@Payams-Mac-mini-2.local>
2018-06-12 22:12:38 +0300

    Authentication Completed

--- server/server.js
+++ server/server.js
@@ -1,26 +1,48 @@
+require('./config/config');
+
 var express = require('express');
 var bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose') ;
-var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+// POST /login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  })
+})
 
 app.listen(port, () => {
   console.log(`started on port ${port}`);
